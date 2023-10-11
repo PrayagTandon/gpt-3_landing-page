160cdaca8810509a3621f07246f96db8ec2a1afd
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-11 17:51:15 +0530

    Starting the navbar development

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 // USING BEM -> BLOCK ELEMENT MODIFIER CSS Class name Notation
 const Navbar = () => {
     return (
         <div className='gpt3__navbar'>
-            <div className='gpt3__navbar-links'></div>
+            <div className='gpt3__navbar-links'>
+                <div className='gpt3__navbar-links_logo'>
+                    <img
+                        src={logo}
+                        alt="logo" />
+                </div>
+            </div>
         </div>
     )
 }
