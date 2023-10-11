40b8a07a7efcf28e4feca4d3b65cc494a2d55f85
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-11 13:04:15 +0530

    Setting up the icons from react-icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 
+// USING BEM -> BLOCK ELEMENT MODIFIER CSS Class name Notation
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='gpt3__navbar'>
+            <div className='gpt3__navbar-links'></div>
+        </div>
     )
 }
 
