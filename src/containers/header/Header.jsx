ab3dc504cbb5ed509dd91dafa158b8cdcf57f509
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-13 10:54:54 +0530

    Styling the header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,7 +5,13 @@ const Header = () => {
     return (
         <header className='gpt3__header section__padding' id='home'>
             <div className='gpt3__header-content'>
-                <h1 className='gradient__text'></h1>
+                <h1 className='gradient__text'>Let’s Build Something amazing with GPT-3 OpenAI</h1>
+                <p className=''></p>
+
+                <div className='gpt3__header-content__input'>
+                    <input type="email" placeholder='Your Email address' />
+                    <button type="button">Get Started</button>
+                </div>
             </div>
         </header>
     )
