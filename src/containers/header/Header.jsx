1822ba5ce1614df58712d54cb87dea36e669d3d8
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-13 10:48:50 +0530

    Settting up the header component

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,11 @@ import './header.scss';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header className='gpt3__header section__padding' id='home'>
+            <div className='gpt3__header-content'>
+                <h1 className='gradient__text'></h1>
+            </div>
+        </header>
     )
 }
 
