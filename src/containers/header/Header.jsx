a7debb70c7a574ec3c61ed14d2d4a866414ec2e2
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-13 11:00:07 +0530

    styling the header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './header.scss';
-
+import '../../styles/_typography.scss';
 const Header = () => {
     return (
         <header className='gpt3__header section__padding' id='home'>
             <div className='gpt3__header-content'>
-                <h1 className='gradient__text'>Let’s Build Something amazing with GPT-3 OpenAI</h1>
+                <h1 className='gradient__text heading__primary'>Let’s Build Something amazing with GPT-3 OpenAI</h1>
                 <p className=''></p>
 
                 <div className='gpt3__header-content__input'>
