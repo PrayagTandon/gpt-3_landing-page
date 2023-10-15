3e0f2551314885a3549ff81fa921fa5fd8d1abe8
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-15 18:58:45 +0530

    Brand section completed

---
+++ src/components/brand/imports.js
@@ -0,0 +1,15 @@
+// This will act as a helper file to import all our images and then we will import all the images to the 'Brand.jsx'.
+
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify,
+}
