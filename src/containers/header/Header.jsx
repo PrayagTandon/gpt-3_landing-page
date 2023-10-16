d500b2cf8f5977253235b1dea50a822023c58e0e
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-16 23:50:43 +0530

    Initial structure finalised

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             {/* HEADER CONTENT */}
 
             <div className='gpt3__header-content'>
-                <h1 className='gradient__text heading__primary'>Let’s Build Something amazing with GPT-3 OpenAI</h1>
+                <h1 className='heading heading__primary'>Let’s Build Something amazing with GPT-3 OpenAI</h1>
                 <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
                 <div className='gpt3__header-content__input'>
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './whatGPT3.scss';
+import { Feature } from '../../components';
 
 const WhatGPT3 = () => {
     return (
-        <section className='gpt3__whatgpt3 section__margin' id='wgpt3'></section>
+        <section className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+            <div className='gpt3__whatgpt3-features'>
+                <Feature />
+            </div>
+
+            <div className='gpt3__whatgpt3-heading'>
+                <h2 className='heading heading__secondary'>
+                    The possibilities are beyond your imagination
+                </h2>
+                <p>Explore The Library</p>
+            </div>
+
+            <div className='gpt3__whatgpt3-container'>
+                <Feature />
+                <Feature />
+                <Feature />
+            </div>
+        </section>
     )
 }
 
