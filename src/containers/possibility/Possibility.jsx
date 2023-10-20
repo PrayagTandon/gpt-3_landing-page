f1283f1f5472bd52cc967fb1479020650f0979ea
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-20 23:07:20 +0530

    Starting the possibility section development

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import possibilityImage from '../../assets/possibility.png'
 import './possibility.scss';
 
 const Possibility = () => {
     return (
-        <section></section>
+        <section className='gpt3__possibility section__padding' id='possibility'>
+            <div className='gpt3__possibility-image'>
+                <img
+                    src={possibilityImage}
+                    alt="Possibility Image" />
+            </div>
+        </section>
     )
 }
 
