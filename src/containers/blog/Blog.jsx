040f7b00d63ab70ad58867cd5364bfc07dadef63
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-21 19:33:55 +0530

    Creating a reusable article component

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Article } from '../../components';
 import './blog.scss';
 
 const Blog = () => {
     return (
-        <div>Blog</div>
+        <section className='gpt3__blog section__padding' id='blog'>
+            <div className='gpt3__blog-heading'>
+                <h2 className='heading heading__secondary'>
+                    A lot is happening,
+                    We are blogging about it.
+                </h2>
+
+                <div className='gpt3__blog-container'>
+                    <div className='gpt3__blog-container_groupA'>
+                        <Article />
+                    </div>
+                    <div className='gpt3__blog-container_groupB'>
+                        <Article />
+                        <Article />
+                        <Article />
+                        <Article />
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
