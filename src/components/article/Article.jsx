8ad8f22540f07f9f81c31bff93c17e1eddfdd78e
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-21 20:23:06 +0530

    Styling the blog container

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -11,10 +11,8 @@ const Article = ({ imgURL, date, title }) => {
             </div>
 
             <div className='gpt3__blog-container_article-content'>
-                <div>
-                    <p>{date}</p>
-                    <h3>{title}</h3>
-                </div>
+                <p>{date}</p>
+                <h3>{title}</h3>
             </div>
 
             <div className='gpt3__blog-container_article-content_btn'>
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -11,17 +11,17 @@ const Blog = () => {
                     A lot is happening,
                     We are blogging about it.
                 </h2>
+            </div>
 
-                <div className='gpt3__blog-container'>
-                    <div className='gpt3__blog-container_groupA'>
-                        <Article imgURL={blog01} date="Sep 28, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-                    </div>
-                    <div className='gpt3__blog-container_groupB'>
-                        <Article imgURL={blog02} date="Sep 22, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-                        <Article imgURL={blog03} date="Sep 14, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-                        <Article imgURL={blog04} date="Aug 30, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-                        <Article imgURL={blog05} date="Aug 26, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-                    </div>
+            <div className='gpt3__blog-container'>
+                <div className='gpt3__blog-container_groupA'>
+                    <Article imgURL={blog01} date="Sep 28, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+                </div>
+                <div className='gpt3__blog-container_groupB'>
+                    <Article imgURL={blog02} date="Sep 22, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+                    <Article imgURL={blog03} date="Sep 14, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+                    <Article imgURL={blog04} date="Aug 30, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+                    <Article imgURL={blog05} date="Aug 26, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
                 </div>
             </div>
         </section>
