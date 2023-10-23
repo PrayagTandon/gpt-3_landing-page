beaae3c30230fdb8040ffca44a63f2efdc9a3201
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-23 19:33:39 +0530

    Starting the footer development

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -3,7 +3,13 @@ import './footer.scss';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className='gpt3__footer section__padding'>
+            <div className='gpt3__footer-heading'>
+                <h2 className='heading heading__secondary heading__footer'>
+                    Do you want to step in to the future before others
+                </h2>
+            </div>
+        </footer>
     )
 }
 
